e7675ea8d9d7a743681d47c42de07b3da84fa733
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 21:03:13 +0200

    gh 0.0.1 fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ca">
       <body className={inter.className}>{children}</body>
     </html>
   )
