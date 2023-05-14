2585ae9a6b524adc10b5d30030dcfd7762c1696f
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:10:03 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,45 +5,27 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Mojo Li 🔥&nbsp;
-          <code className="font-mono font-bold">Educative Programming language for all of AI site</code>
+          Mojo Cat 🔥&nbsp;
+          <code className="font-mono font-bold">Mojo Programming language educative site</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          Read the code on&nbsp;
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.modular.com/mojo"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
+            href="https://github.com/miguelgargallo/mojo-cat"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <a
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
-              href="https://www.modular.com/mojo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              MOJO 🔥
-            </a>
-            &nbsp;+&nbsp;
-            <a
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
-              href="https://pylar.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pylar AI
-            </a>
+            GitHub
           </a>
         </div>
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="relative z-10 text-6xl font-bold text-center text-gray-900 dark:text-gray-100 lg:text-8xl">
           <span className="block">Welcome to</span>
           <span className="block">Mojo.li 🔥 + Pylar AI</span>
         </h1>
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://www.modular.com/mojo"
@@ -61,7 +43,6 @@ export default function Home() {
             Discover Mojo here.
           </p>
         </a>
-
         <a
           href="https://pylar.org"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
@@ -78,7 +59,6 @@ export default function Home() {
             Discover Pylar AI, the power of AI for your business.
           </p>
         </a>
-
         <a
           href="https://mojo.li"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -93,7 +73,6 @@ export default function Home() {
             We are Mojo Li too!
           </p>
         </a>
-
         <a
           href="https://twitter.com/miguelgargallo"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
