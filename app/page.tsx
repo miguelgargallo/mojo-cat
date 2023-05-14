8d803024634566c21c285ce3c182d51d248da3d9
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:36:39 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Mojo Cat 🔥&nbsp;
-          <code className="font-mono font-bold hidden lg:inline">Mojo Programming language educative site</code>
+          <code className="font-mono font-bold hidden lg:inline">Aprén el nou llenguatge de programació Mojo en català.</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Read the code on&nbsp;
+          Som opensource a&nbsp;
           <a
             className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
             href="https://github.com/miguelgargallo/mojo-cat"
@@ -20,8 +20,8 @@ export default function Home() {
       </div>
       <div className="py-8 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="relative z-10 text-6xl font-bold text-center text-gray-900 dark:text-gray-100 lg:text-8xl">
-          <span className="block">Under</span>
-          <span className="block">construction</span>
+          <span className="block">Aviat</span>
+          <span className="block">Online</span>
         </h1>
       </div>
       <div className="py-8 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -32,13 +32,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Read more 🔥{' '}
+            Llegeix mes 🔥{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover the official Mojo site by Modular.
+            Descobreix el lloc oficial de Modular.
           </p>
         </a>
         <a
@@ -54,7 +54,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover Pylar AI, the power of AI for your business.
+            Integrem Pylar AI verticalment al teu negoci.
           </p>
         </a>
         <a
@@ -68,7 +68,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            We are Mojo Li too!
+            Som propietaries de la marca Mojo.li també.
           </p>
         </a>
         <a
@@ -84,7 +84,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Mojo 🔥 Cat. 2023 All rights reserved. mojo.cat is not an official site or affiliated with Modular.
+            Mojo 🔥 Cat. 2023 Tots els drets reservats. mojo.cat no és un lloc oficial ni afiliat a Modular.
           </p>
         </a>
       </div>
