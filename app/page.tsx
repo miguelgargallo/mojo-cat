bda9f53db4efbed1ceacaea957cfd81982baf1f0
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:38:02 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -62,13 +62,13 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Mojo.li 🔥 {' '}
+            Mojo.li 🔥 ES {' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Som propietaries de la marca Mojo.li també.
+            Mojo en castellà.
           </p>
         </a>
         <a
